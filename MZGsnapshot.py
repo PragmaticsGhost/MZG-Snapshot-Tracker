1ae5e2662aac2a28ad0e7c8069f4e36703f5318b
cannabeatz <nickd@eliteaudio1.com>
2022-05-17 00:12:25 -0400

    v1.0

--- MZGsnapshot.py
+++ MZGsnapshot.py
@@ -2,95 +2,191 @@ import requests
 import json
 import time
 from datetime import datetime
-import os
+from etherscan import Etherscan
 
 
 loop = True
 
 while loop:
 
-    
-    walletID = "0x5558AED91F04C83bb9dd7201181634997D96a912" #input("Please enter the Wallet ID you would like to query the OpenSea API about:\n") Faster for testing 
-    contractID = "0xA0529c325e2594dcc599BA6E39aA4d6b28834c53" #input("Please enter the Contract ID you would like to query:\n") Faster for testing 
-    snapshotDate = "2022-02-28 11:59" #str(input('Enter date(yyyy-mm-dd hh:mm): ')) Faster for testing 
-
+    es = Etherscan('UVCMJ2PJ8CRISDAKNC7GB7I62K38U7G2DP')
+    walletID = input("Please enter the Wallet ID you would like to query the OpenSea API about:\n") #TESTING
+    snapshotDate = str(input('Enter date you would like to check eligibility on/before(yyyy-mm-dd hh:mm): ')) #TESTING
+    print("")
     dt = datetime.strptime(snapshotDate, '%Y-%m-%d %H:%M')
     epoch = dt.timestamp() 
-    print(epoch)
+    epochFloat = epoch
+    epochString = str(epochFloat)
+    correctedEpoch = epochString.replace(".0","")
+    
 
 
+    print("1 = MetaZoo Games Tokens")
+    print("2 = Metazoo Genesis\n")
+    contractID = input("Please select the Contract ID you would like to query:\n") #TESTING
+    if contractID == "1":
+        contractIDMZG = "0xA0529c325e2594dcc599BA6E39aA4d6b28834c53"
+        
 
-    walletLength = len(walletID)
-    wallet1stchars = walletID[0:2]
-    print("Requested Wallet ID is:" + walletID + ", is this correct?\n")
-    time.sleep(0.3)
-    answer = "Y" #input("Y/N?:\n") Faster for testing 
+        walletLength = len(walletID)
+        wallet1stchars = walletID[0:2]
+        print("Requested Wallet ID is:" + walletID + ", is this correct?\n")
+        time.sleep(0.3)
+        answer = input("Y/N?:\n") #TESTING
 
-    if answer == "Y":
+        if answer == "Y":
 
-        if walletLength == 42 : 
+            if walletLength == 42 : 
 
-            if wallet1stchars == "0x" :
+                if wallet1stchars == "0x" :
 
-                print("Wallet ID appears to be valid.\n")
-                time.sleep(0.3)
-                print("Querying OpenSea & Etherscan APIs for Wallet info...\n")
-                time.sleep(0.3)
+                    print("Wallet ID appears to be valid.\n")
+                    time.sleep(0.3)
+                    print("Querying OpenSea & Etherscan APIs for Wallet info...\n")
+                    time.sleep(0.3)
 
-                #responseOS = requests.get(f"https://api.opensea.io/api/v1/collections?asset_owner={walletID}&limit=300")
-                #responseES2 = requests.get(f"https://api.etherscan.io/api?module=account&action=tokennfttx&contractaddress={contractID}&address={walletID}&page=1&offset=100&startblock=0&endblock=27025780&sort=asc&apikey=UVCMJ2PJ8CRISDAKNC7GB7I62K38U7G2DP")
+                    getERC721TxEvents = es.get_erc721_token_transfer_events_by_address(address=(walletID), startblock=0, endblock=27025780, sort="asc")
+             
+                    print("Done.\n")
 
-                print("Done.")
-                # Future If statement to evaluate previous snapshot state
-                #print("Snapshotting chain, please wait...\n")
-                #os.system("erc721-snapshot")
+                    with open('resultES2.json', "w") as outfile:   
+                        json.dump(getERC721TxEvents, outfile, indent=2)
 
-                #prettyOS = json.dumps(responseOS.json(), indent=2)
-                #prettyES2 = json.dumps(responseES2.json(), indent=2)
+                    print('Etherscan Snapshot Data for Wallet ID ' + walletID + ' at ' + snapshotDate + ' successfully written to "Desktop/resultES2.json"\n')
 
-                #resOS = open('resultOS.json', mode='w')
-                #resES2 = open('resultES2.json', mode='w')
+                    time.sleep(0.3)
+                    print("Iterating through JSON object to enumerate Wallet Token ID's....\n")
 
-                #resOS.write(prettyOS)
-                #resES2.write(prettyES2)
+                    with open('resultES2.json', 'r') as f:   
+                        resultES2_dict = json.load(f) 
 
-                #print('OpenSea Collection Snapshot Data for Wallet ID ' + walletID + ' successfully written to "Desktop/resultOS.json"\n')
-                print('Etherscan Snapshot Data for Wallet ID ' + walletID + ' successfully written to "Desktop/resultES2.json"\n')
+                        for toAddress in resultES2_dict:
 
+                            recvAddr = toAddress.get('to') 
+                            recvTime = toAddress.get('timeStamp')
+                            contractAddr = toAddress.get('contractAddress')
+                            intRecvTime = int(recvTime)
+                            correctedDate = datetime.fromtimestamp(intRecvTime)
+                            recvTimeStr = str(recvTime)
+                            
 
-                time.sleep(0.3)
-                print("Iterating through JSON object to enumerate Wallet Token ID's....\n")
+                            #print("recvAddr")                          #DEBUG
+                            #print(recvAddr.casefold(), len(recvAddr))  #DEBUG
+                            #print("")                                  #DEBUG
+                            #print("walletID")                          #DEBUG
+                            #print(walletID.casefold(), len(walletID))  #DEBUG
+                            #rint("")                                   #DEBUG
+                            #print("contractAddr")                      #DEBUG
+                            #print(contractAddr.casefold(), len(contractAddr)) #DEBUG
+                            #print("")                                  #DEBUG
+                            #print("contractIDMZG")                     #DEBUG
+                            #print(contractIDMZG.casefold(), len(contractIDMZG)) #DEBUG
+                            #print("")                                  #DEBUG
+                            #print("recvTimeStr")                       #DEBUG
+                            #print(recvTimeStr, len(recvTimeStr))  #DEBUG
+                            #print("")                             #DEBUG
+                            #print("correctedEpoch")               #DEBUG
+                            #print(correctedEpoch, len(correctedEpoch)) #DEBUG      
+                            #print("")                              #DEBUG
 
-                with open('resultES2.json', 'r') as f:
-                    
-                    resultES2_dict = json.load(f)
-                    
+                            if recvAddr.casefold() == walletID.casefold():
+                                if contractAddr.casefold() == contractIDMZG.casefold():
+                                    if recvTimeStr <= correctedEpoch:
 
-                    for toAddress in resultES2_dict:
-
-
-                        recvAddr = toAddress.get('to') 
-                        recvTime = toAddress.get('timeStamp')
-                        intRecvTime = int(recvTime)
-                        correctedDate = datetime.fromtimestamp(intRecvTime)
-
-                        #print(recvAddr.casefold(), len(recvAddr))  DEBUG
-                        #print(walletID.casefold(), len(walletID))  DEBUG
-                    
-                        if recvAddr.casefold() == walletID.casefold():
-
-
-                            print("The following MZG Token ID's entered this wallet at the following times")    
-                            print(toAddress['tokenID'], correctedDate)
+                                        print("The following MZG Genesis TokenID's entered this wallet before " + snapshotDate + " at the following times:")    
+                                        print(toAddress['tokenID'], correctedDate)
+                                        print("")
                         
-                            loop = False   
+                                    loop = False   
+
                     loop = False          
 
+                else:
+                    print('Wallet ID does not start with "0x", please make sure there is an "0x" at the beginning of the wallet address.\n')           
+
             else:
-                print('Wallet ID does not start with "0x", please make sure there is an "0x" at the beginning of the wallet address.\n')           
+                print("Wallet ID is not the correct number of characters, try again.\n")
 
         else:
-            print("Wallet ID is not the correct number of characters, try again.\n")
+            print("Incorrect wallet address, try again.\n")        
+    elif contractID == "2":
+        contractIDMZGT = "0x2D366Be8fA4D15c289964dD4Adf7Be6Cc5e896E8"
+
+        walletLength = len(walletID)
+        wallet1stchars = walletID[0:2]
+        print("Requested Wallet ID is:" + walletID + ", is this correct?\n")
+        time.sleep(0.3)
+        answer = input("Y/N?:\n") #TESTING
+
+        if answer == "Y":
+
+            if walletLength == 42 : 
+
+                if wallet1stchars == "0x" :
+
+                    print("Wallet ID appears to be valid.\n")
+                    time.sleep(0.3)
+                    print("Querying OpenSea & Etherscan APIs for Wallet info...\n")
+                    time.sleep(0.3)
+
+                    getERC721TxEvents = es.get_erc721_token_transfer_events_by_address(address=(walletID), startblock=0, endblock=27025780, sort="asc")
+             
+                    print("Done.\n")
+
+                    with open('resultES2.json', "w") as outfile:   
+                        json.dump(getERC721TxEvents, outfile, indent=2)
+
+                    print('Etherscan Snapshot Data for Wallet ID ' + walletID + ' at ' + snapshotDate + ' successfully written to "Desktop/resultES2.json"\n')
+
+                    time.sleep(0.3)
+                    print("Iterating through JSON object to enumerate Wallet Token ID's....\n")
+
+                    with open('resultES2.json', 'r') as f:   
+                        resultES2_dict = json.load(f) 
+
+                        for toAddress in resultES2_dict:
+
+                            recvAddr = toAddress.get('to') 
+                            recvTime = toAddress.get('timeStamp')
+                            contractAddr = toAddress.get('contractAddress')
+                            intRecvTime = int(recvTime)
+                            correctedDate = datetime.fromtimestamp(intRecvTime)
+                            recvTimeStr = str(recvTime)
+                            
+
+                            #print("recvAddr")                          #DEBUG
+                            #print(recvAddr.casefold(), len(recvAddr))  #DEBUG
+                            #print("")                                  #DEBUG
+                            #print("walletID")                          #DEBUG
+                            #print(walletID.casefold(), len(walletID))  #DEBUG
+                            #rint("")                                   #DEBUG
+                            #print("contractAddr")                      #DEBUG
+                            #print(contractAddr.casefold(), len(contractAddr)) #DEBUG
+                            #print("")                                  #DEBUG
+                            #print("contractIDMZG")                     #DEBUG
+                            #print(contractIDMZG.casefold(), len(contractIDMZG)) #DEBUG
+                            #print("")                                  #DEBUG
+                            #print("recvTimeStr")                       #DEBUG
+                            #print(recvTimeStr, len(recvTimeStr))  #DEBUG
+                            #print("")                             #DEBUG
+                            #print("correctedEpoch")               #DEBUG
+                            #print(correctedEpoch, len(correctedEpoch)) #DEBUG      
+                            #print("")                              #DEBUG
+
+                            if recvAddr.casefold() == walletID.casefold():
+                                if contractAddr.casefold() == contractIDMZGT.casefold():
+                                    if recvTimeStr <= correctedEpoch:
+
+                                        print("The following MZGT TokenID's entered this wallet before " + snapshotDate + " at the following times:\n")    
+                                        print(toAddress['tokenID'], correctedDate)
+                                        print("")
+                        
+                                    loop = False   
+
+                    loop = False  
+
+
 
     else:
-        print("Incorrect wallet address, try again.\n")        
+        print("Error, only enter 1 or 2...")    
+
---
+++ pyapy.py
@@ -0,0 +1,5 @@
+from etherscan import Etherscan
+
+eth = Etherscan('VCMJ2PJ8CRISDAKNC7GB7I62K38U7G2DP')
+
+eth.get_gas_oracle()
